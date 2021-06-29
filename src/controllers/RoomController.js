b05568c2424e90db5acc407a5e85e4e99166549b
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2021-06-28 23:04:37 -0300

    Criação do DB de rooms e questions

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,28 @@
-module.exports = {
-    create(req, res){
+const Database = require('../db/config')
 
-        let roomId = 123456
+module.exports = {
+    async create(req, res){
+        const db = await Database()
+        const pass = req.body.password
+        let roomId = ''
+
+        for(let i = 0; i < 6; i++){
+           roomId += Math.floor(Math.random() * 10).toString()
+        }
+
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        )VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
+        
+        await db.close()
+        
 
         res.redirect(`/room/${roomId}`)
     }
 }
+
